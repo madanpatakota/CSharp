1824149016d7fb87448e55823989dc52e01a0eb0
Madan.patakota <madan.patakota@gmail.com>
2025-06-13 09:46:37 +0530

    Add project files.

---
+++ CSharp_Step1/1.Basics.cs
@@ -0,0 +1,56 @@
+﻿using System;
+class FirstProgram
+{
+    static void Main()
+    {
+        Console.WriteLine("-----------------------------------------------------");
+
+        Console.WriteLine("*********Main method *********");
+
+        Console.WriteLine("My First Message i.e. Hello World");
+
+        Console.WriteLine("For read the Message Please enter any name");
+        
+
+        string value = Console.ReadLine();  //Stop   -- Its waitingng for your word
+
+        Console.WriteLine("Name is: " + value);
+
+        Console.WriteLine("-----------------------------------------------------");
+
+        DisplayMessages();
+
+        Console.WriteLine("-----------------------------------------------------");
+
+        ReadMessages(); //calling method
+    }
+
+
+    static void DisplayMessages()
+    {
+        Console.WriteLine("*********DisplayMessages method *********");
+
+        Console.WriteLine("Please enter the first name ? ");
+        string firstName =  Console.ReadLine();
+
+        Console.WriteLine("Please enter the last name ? ");
+        string lastName = Console.ReadLine();
+
+        string FullName = firstName + lastName;
+        Console.WriteLine("Your Full Name is: " + FullName);
+
+    }
+        
+
+
+
+    //Defination
+    static void ReadMessages()
+    {
+        Console.WriteLine("Read Message");
+    }
+
+
+
+}
+
