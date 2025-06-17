5abc483de09004a998075fb35f7fc55141e31b86
Madan.patakota <madan.patakota@gmail.com>
2025-06-17 09:37:36 +0530

    string

---
+++ CSharp_Step1/4.VarAndDynamic.cs
@@ -0,0 +1,69 @@
+﻿using System;
+class VarAndDynamic
+{
+    static void Main()
+    {
+
+
+        //
+
+        //
+        //var and dynamic keywords
+
+        //int age = 20;
+        //age = 30;
+        //Console.WriteLine(age.GetType()); // System.Int32
+
+        //string compnayName = "Misard";
+        //compnayName = "TCS";
+        //Console.WriteLine(compnayName.GetType()); // System.String
+
+        //double price = 13.78d;
+        //price = 50.78d;
+
+        //Console.WriteLine(price.GetType());
+
+
+        //var first = "Hello , world";
+
+        //var v_number = 10;         // compliler int
+        //Console.WriteLine(v_number.GetType()); // System.Int32
+
+        //var v_name = "Misard";     // compliler string
+        //Console.WriteLine($"v_name Before TCS {v_name.Length}");    //  5
+        ////    v_name = 10;
+
+        //v_name = "TCS";
+        //Console.WriteLine(v_name.GetType()); // System.Int32 
+        //Console.WriteLine($"v_name {v_name.Length}");
+
+        //var v_price = 12.5d;        // compliler double
+        //Console.WriteLine(v_price.GetType()); // System.Int32
+
+
+        //Dynamic 
+
+
+        dynamic second;
+
+
+        dynamic d_number = 10;         // compliler int
+        Console.WriteLine(d_number.GetType()); // System.Int32
+
+        dynamic d_name = "Misard";     // compliler string
+        //d_name = 10;
+        Console.WriteLine($"v_name Before TCS {d_name.Length}");    //  5
+        //    v_name = 10;
+
+        d_name = "TCS";
+        Console.WriteLine(d_name.GetType()); // System.Int32 
+        //d_name = 56555;
+
+        Console.WriteLine($"v_name {d_name.Length}");
+
+        dynamic d_price = 12.5d;        // compliler double
+        d_price = "Misard";
+        Console.WriteLine(d_price.GetType()); // System.Int32
+
+    }
+}
---
+++ CSharp_Step1/5.String.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class StringExample
+{
+    static void Main()
+    {
+        string strCourseName = "Csharp";   // Csharp
+
+        string strCourseName1 = "\"Csharp\"";  // "Csharp"
+        Console.WriteLine(strCourseName1);
+
+        string numberStrings = "One.\nTwo.\nThree";  // "Csharp"
+        Console.WriteLine(numberStrings);
+
+        //string myProjectPath = @"D:\Csharp - Training\CSharp\CSharp_Step1";
+        string myProjectPath = @"D:\Csharp - Training\\CSharp\\CSharp_Step1\";
+        Console.WriteLine(myProjectPath);
+
+        //Interpolation
+        string name = "Alice";
+        int age = 30;
+        string message = $"My name is {name} and i am {age} yeals old";
+
+        Console.WriteLine(message);
+        //$""
+    }
+
+}
+
